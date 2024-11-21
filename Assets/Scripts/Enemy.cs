6acaefa1f26f5eed7d9c748dd4bf6a07d5cc1ec2
Fabian Lundberg <fabian.lundberg1@learnet.se>
2024-11-21 10:42:09 +0100

    EnemyScript

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public bool flip;
+    public float speed; 
+
+    private void Update()
+    {
+        Vector3 scale = transform.localScale;
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1) ;
+            transform.Translate(x:speed * Time.deltaTime, y: 0, z: 0);
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
+            transform.Translate(x:speed * Time.deltaTime * -1, y: 0, z: 0);
+        }
+
+        transform.localScale = scale;
+    }
+}
