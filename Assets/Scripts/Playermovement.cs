6d43c86e769b7e4fcd176571fa84c50d816f113f
TheUndeadElite <almir.bajraktarevic@elev.ga.lbs.se>
2024-11-28 10:47:36 +0100

    fixed hp script

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -6,15 +6,19 @@ public class Playermovement : MonoBehaviour
     [SerializeField] float jumpForce;
     [SerializeField] bool isGrounded;
     [SerializeField] float groundCheckRadius;
+    bool facingRight = true;
+    [SerializeField] GameObject gun;
 
     [SerializeField] Transform groundcheck;
     [SerializeField] LayerMask groundLayer;
 
     Rigidbody2D rb;
     Vector2 playerInput;
+    SpriteRenderer SpriteRenderer;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        SpriteRenderer = rb.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -28,6 +32,15 @@ public class Playermovement : MonoBehaviour
             
         }
         isGrounded = Physics2D.OverlapCircle(groundcheck.position, groundCheckRadius, groundLayer);
+
+        if (playerInput.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (playerInput.x < 0 && facingRight)
+        {
+            Flip();
+        }
     }
     private void FixedUpdate()
     {
@@ -38,5 +51,21 @@ public class Playermovement : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundcheck.position, groundCheckRadius);
+    }
+
+    void Flip()
+    {
+        
+        facingRight = !facingRight;
+
+        
+        if (facingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
     }
 }
