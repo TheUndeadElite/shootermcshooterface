18cac0b7eb1449172734ef93cda5a8af769babbd
TheUndeadElite <almir.bajraktarevic@elev.ga.lbs.se>
2024-12-12 08:34:04 +0100

    bullet done

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,13 +5,12 @@ public class Bullet : MonoBehaviour
     public float speed = 20f;
     public int damage = 25;
     public Rigidbody2D rb;
-    public Animator impactEffect;
 
     public float spreadAngle = 5f;
 
     void Start()
     {
-        impactEffect = GetComponent<Animator>();
+        
         float randomSpread = Random.Range(-spreadAngle, spreadAngle);
         Vector2 direction = Quaternion.Euler(0, 0, randomSpread) * transform.right;
 
@@ -21,14 +20,18 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D hitInfo)
     {
         EnemyHealth enemy = hitInfo.GetComponent<EnemyHealth>();
-        if (enemy != null)
+        if (enemy != null && hitInfo.tag == "Enemy")
         {
             enemy.TakeDamage(damage);
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
+        else
+        {
+            Destroy(gameObject, 2f);
+        }
     }
     void Update()
     {
-        Destroy(gameObject, 2f);
+
     }
 }
